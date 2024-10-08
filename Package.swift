002289123342139847c1fcf19b155886d5272461
phinm <phinm@outlook.com>
2024-10-08 22:51:26 +0700

    Remove platforms target.

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "BigDecimal",
-    platforms: [
+    /*platforms: [
         .macOS("15.0"), .iOS("18.0"), .macCatalyst("15.0"), .tvOS("18.0"),
         .watchOS("11.0")
-    ],
+    ],*/
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
